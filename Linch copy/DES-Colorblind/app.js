9a1dbeaedfe7aaa377172c9d1417ad7b0e60a1f2
Supawee Rattanajitdamrong <supaweeratt@gmail.com>
2020-08-30 13:13:51 +0700

    Update DES provider info page
    Fix result alignment in .css
    change the font to Ubuntu

---
+++ Linch copy/DES-Colorblind/app.js
@@ -0,0 +1,120 @@
+new Vue({
+  el: "#vue-app",
+  data() {
+    return {
+      homepage: "HOME PAGE",
+      infoList: [],
+      showList: [],
+      nameList: [{ value: "All Providers" }],
+      specialityList: [{ value: "All Specialities" }],
+      ratingList: [
+        { value: "Rating" },
+        { value: 5 },
+        { value: 4 },
+        { value: 3 },
+        { value: 2 },
+        { value: 1 },
+      ],
+      postInput: "",
+      providerName: "All Providers",
+      specialityName: "All Specialities",
+      rating: "Rating",
+    };
+  },
+  methods: {
+    // handleNameSelected(selectedName) {
+    //   // console.log(selectedName);
+    //   if(selectedName!="All Providers"){}
+    //   for(let item of infoList){
+    //   }
+    // }
+    // },
+    handleSearch() {
+      var requestOptions1 = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      console.log("click");
+      this.infoList = [];
+      this.showList = [];
+
+      fetch(
+        `https://g7n5ifjzkj.execute-api.us-east-1.amazonaws.com/api/search?name=${this.providerName}&speciality=${this.specialityName}&rating=${this.rating}&postal=${this.postInput}`,
+        requestOptions1
+      )
+        .then((response) => response.text())
+        .then((result) => {
+          this.infoList = JSON.parse(result).All_Info;
+          this.showList = this.infoList;
+
+          // // remove duplicated values
+          // names = [...new Set(names)];
+          // names.sort();
+          // // console.log(names);
+          // specialities = [...new Set(specialities)];
+          // specialities.sort();
+
+          // //form as json list
+          // for (let info of names) {
+          //   this.nameList.push({ value: info });
+          // }
+
+          // for (let info of specialities) {
+          //   this.specialityList.push({ value: info });
+          // }
+
+          // console.log(this.nameList);
+          // console.log(this.specialityList);
+          // console.log("info:", this.infoList);
+          // console.log(this.homepage);
+        })
+        .catch((error) => console.log("error", error));
+    },
+  },
+  mounted() {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://g7n5ifjzkj.execute-api.us-east-1.amazonaws.com/api/allinfo",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        this.infoList = JSON.parse(result).All_Info;
+        this.showList = this.infoList;
+        // console.log(this.infoList);
+        // let names = [];
+        // let specialities = [];
+        let names = JSON.parse(result).Name_List;
+        let specialities = JSON.parse(result).Speciality_List;
+        names.sort();
+        // names.map((name) => ({ value: name, label: name }));
+        specialities.sort();
+        // specialities.map((speciality) => ({
+        //   value: speciality,
+        //   label: speciality,
+        // }));
+
+        // this.nameList = names;
+        // this.specialityList = specialities;
+
+        for (let info of names) {
+          // console.log(info);
+          this.nameList.push({ value: info });
+        }
+
+        for (let info of specialities) {
+          this.specialityList.push({ value: info });
+        }
+
+        console.log(this.nameList);
+        console.log(this.specialityList);
+        // console.log(this.homepage);
+      })
+      .catch((error) => console.log("error", error));
+  },
+});
