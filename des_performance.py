37757b147ac8132d8eee79dc947ad1ad0ef05e58
Supawee Rattanajitdamrong <supaweeratt@gmail.com>
2020-10-09 15:49:15 +0700

    Change month format and add new column for Y-M

--- des_performance.py
+++ des_performance.py
@@ -252,6 +252,17 @@ for index, row in data.iterrows():
         data.loc[index, 'Commenced_Ongoing'] * 100 / data.loc[index, 'Commenced'])
 
 
+    # create a column to be parsed as date format in D3
+    data.loc[index,'Year-Month'] = str(data.loc[index,'Year'])+'-'+str(data.loc[index, 'Month'])
+
+
+# change month from number to word to show on the website
+month2word = {1:'January', 2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',\
+             10:'October',11:'November',12:'December'}
+
+data.replace({'Month':month2word},inplace=True)
+
+
 # save to csv
 data.to_csv('Dataset/DES_PERFORMANCE.csv', index=False)
 
