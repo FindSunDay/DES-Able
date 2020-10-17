f5b91aa70c68a794341271ef6194d78119753467
FindSunDay <stayfightfirst@gmail.com>
2020-10-18 01:02:14 +1100

    Apply button to auto generate current location

--- iterations/iteration3/DES-Normal/app.js
+++ iterations/iteration3/DES-Normal/app.js
@@ -63,7 +63,8 @@ new Vue({
     //   }
     // }
     // },
-        // 点击图片回到顶部方法，加计时器是为了过渡顺滑
+    
+    // Click on the picture to go back to the top method, adding a timer is for smooth transition
     backTop () {
         const that = this
         let timer = setInterval(() => {
@@ -75,7 +76,7 @@ new Vue({
         }, 16)
     },
 
-    // 为了计算距离顶部的高度，当高度大于60显示回顶部图标，小于60则隐藏
+    // In order to calculate the height from the top, when the height is greater than 60, the top icon will be displayed, and the icon will be hidden if it is less than 60.
     scrollToTop () {
       const that = this
       let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
@@ -261,11 +262,11 @@ new Vue({
         .catch((error) => console.log("error", error));
     },
     handleSearchNearby() {
-      try {
-        this.fillInAddress();
-      } catch {
-        this.null_des_site = true;
-      }
+      // try {
+      //   this.fillInAddress();
+      // } catch {
+      //   this.null_des_site = true;
+      // }
       console.log("123", this.entered_address, this.specialityName);
       if (this.entered_address != "" && this.specialityName != "") {
         var requestOptions = {
@@ -443,27 +444,8 @@ new Vue({
         center: center,
       });
       console.log(map);
-      // const contentString =
-      //   '<div id="content">' +
-      //   '<div id="siteNotice">' +
-      //   "</div>" +
-      //   '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-      //   '<div id="bodyContent">' +
-      //   "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      //   "sandstone rock formation in the southern part of the " +
-      //   "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      //   "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      //   "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      //   "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      //   "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      //   "Aboriginal people of the area. It has many springs, waterholes, " +
-      //   "rock caves and ancient paintings. Uluru is listed as a World " +
-      //   "Heritage Site.</p>" +
-      //   '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      //   "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      //   "(last visited June 22, 2009).</p>" +
-      //   "</div>" +
-      //   "</div>";
+
+
       const infowindow = new google.maps.InfoWindow({
         // content: contentString,
         content: name,
@@ -496,6 +478,33 @@ new Vue({
       });
       console.log(provider);
       // this.initAutocomplete();
+      
+      // const directionsRenderer = new google.maps.DirectionsRenderer();
+      // const directionsService = new google.maps.DirectionsService();
+      // directionsRenderer.setMap(map)
+      // this.calculateAndDisplayRoute(directionsService, directionsRenderer,lat, lng);
+    },
+    calculateAndDisplayRoute(directionsService, directionsRenderer, lat, lng) {
+      // const selectedMode = document.getElementById("mode").value;
+      console.log("123", lat, lng)
+      const selectedMode = "DRIVING";
+      directionsService.route(
+        {
+          origin: { lat: this.des_site_five.cur_lat, lng: this.des_site_five.cur_lng },
+          destination: { lat: 37.768, lng: -122.511 },
+          // Note that Javascript allows us to access the constant
+          // using square brackets and a string value as its
+          // "property."
+          travelMode: google.maps.TravelMode[selectedMode],
+        },
+        (response, status) => {
+          if (status == "OK") {
+            directionsRenderer.setDirections(response);
+          } else {
+            window.alert("Directions request failed due to " + status);
+          }
+        }
+      );
     },
     initAutocomplete() {
       // Create the autocomplete object, restricting the search predictions to
@@ -532,8 +541,68 @@ new Vue({
       console.log("str", str);
       this.entered_address = str;
     },
+    getAddress(){
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            // let pos = {
+            //   lat: position.coords.latitude,
+            //   lng: position.coords.longitude,
+            // };
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent("Location found.");
+            // infoWindow.open(map);
+            // map.setCenter(pos);
+            // console.log("current location:",pos)
+            var requestOptions = {
+              method: 'GET',
+              redirect: 'follow'
+            };
+            
+            fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyDtU4wnc7N3-U9QMpRCG5CCaqCJc2nYuz8&language=en_AU`, requestOptions)
+              .then(response => response.text())
+              .then(result => {
+                // console.log("address",result);
+                let res = JSON.parse(result);
+                // console.log("address", res.results[0].formatted_address);
+                this.entered_address = res.results[0].formatted_address;
+            })
+              .catch(error => console.log('error', error));
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          }
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    },
   },
   mounted() {
+
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(
+    //     (position) => {
+    //       let pos = {
+    //         lat: position.coords.latitude,
+    //         lng: position.coords.longitude,
+    //       };
+    //       // infoWindow.setPosition(pos);
+    //       // infoWindow.setContent("Location found.");
+    //       // infoWindow.open(map);
+    //       // map.setCenter(pos);
+    //       console.log("current location:",pos)
+    //     },
+    //     () => {
+    //       handleLocationError(true, infoWindow, map.getCenter());
+    //     }
+    //   );
+    // } else {
+    //   // Browser doesn't support Geolocation
+    //   handleLocationError(false, infoWindow, map.getCenter());
+    // }
+
     window.addEventListener('scroll', this.scrollToTop)
     // this.initMap();
     var requestOptions = {
